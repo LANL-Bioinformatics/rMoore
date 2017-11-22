d8e1fa0e36eed39cff9fdf38b635b82950147dd5
Geoffrey House <ghouse@pn1704855.lanl.gov>
2017-11-22 14:01:49 -0700

    Updated server.R to make searches more comprehensive (searching all PubMedIDs linked to all input accession numbers), genome papers to be consistently listed at the top of search results, and more robust parsing of accession entries in the search bar of the app. No changes made to the other files.

---
+++ ui.R
@@ -0,0 +1,30 @@
+# Load the ggplot2 package which provides
+# the 'mpg' dataset.
+
+fluidPage(
+  ## hack to  align action button  with textInput box  # use 25px if textInput label != ""
+  tags$head(   tags$style(type='text/css', 'button#update{ margin-top: 20px;}') ),
+  titlePanel( 'Search biomedical literature in Europe PMC'),
+  #helpText("Multiple accession ids must be separated by ';'"),
+  
+  # Original
+  # fluidRow( column(10, textInput("acc_num",   value= "" , width= "100%", label= "", placeholder="accession number(s); multiple accession numbers must be separated by space" )) ,
+  #           # color like submitButton  
+  #           column(2, actionButton("update", "Search",  style="color: #fff; background-color: #337ab7; border-color: #2e6da4" ))
+  #           #column(2, numericInput("obs", "Number of articles", 100 ))
+  #           ),
+  
+  # Modified to change default message in the text search field
+  fluidRow( column(10, textInput("acc_num",   value= "" , width= "100%", label= "", placeholder="accession number(s); separate multiple accession numbers using spaces or commas" )) ,
+            # color like submitButton
+            column(2, actionButton("update", "Search",  style="color: #fff; background-color: #337ab7; border-color: #2e6da4" ))
+            #column(2, numericInput("obs", "Number of articles", 100 ))
+            ),
+  
+  br(),
+  
+  tabsetPanel(type = "tabs", 
+              tabPanel("Articles",    DT::dataTableOutput("table")),
+              tabPanel("Plot",     dygraphs::dygraphOutput('plot') )         
+  )  
+)
